f22bf2fee0416630f45c9ebb91c15291d2a2db3c
sunakan <sunakan03@gmail.com>
2019-05-02 09:35:46 +0900

    [Add] bin/rails db:create した後に、 db:migrate

--- blorgh/test/dummy/db/schema.rb
+++ blorgh/test/dummy/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_220453) do
+ActiveRecord::Schema.define(version: 2019_05_02_003337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blorgh_comments", force: :cascade do |t|
+    t.integer "article_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
