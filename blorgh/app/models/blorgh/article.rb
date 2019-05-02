c1fc2600a244e5d30f27bee7495d08b4eadc13d8
sunakan <sunakan03@gmail.com>
2019-05-02 11:01:06 +0900

    [Add] 4.3のアプリケーションのクラスをエンジンで使うの最後の方まで

--- blorgh/app/models/blorgh/article.rb
+++ blorgh/app/models/blorgh/article.rb
@@ -1,5 +1,15 @@
 module Blorgh
   class Article < ApplicationRecord
+    attr_accessor :author_name
+    belongs_to :author, class_name: "User"
+
+    before_validation :set_author
+
     has_many :comments
+
+    private
+      def set_author
+        self.author = User.find_or_create_by(name: author_name)
+      end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_014115) do
+ActiveRecord::Schema.define(version: 2019_05_02_015255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_05_02_014115) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
   create_table "blorgh_comments", force: :cascade do |t|
