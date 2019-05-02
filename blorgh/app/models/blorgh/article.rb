65b46c2c03369fd129136996508d2d1eb6566d2f
sunakan <sunakan03@gmail.com>
2019-05-02 12:35:09 +0900

    [Add] 4.4の　「エンジンの設定をする」を最後まで

--- blorgh/app/models/blorgh/article.rb
+++ blorgh/app/models/blorgh/article.rb
@@ -1,7 +1,7 @@
 module Blorgh
   class Article < ApplicationRecord
     attr_accessor :author_name
-    belongs_to :author, class_name: "User"
+    belongs_to :author, class_name: Blorgh.author_class.to_s
 
     before_validation :set_author
 
@@ -9,7 +9,7 @@ module Blorgh
 
     private
       def set_author
-        self.author = User.find_or_create_by(name: author_name)
+        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
       end
   end
 end
--- blorgh/lib/blorgh.rb
+++ blorgh/lib/blorgh.rb
@@ -1,5 +1,13 @@
 require "blorgh/engine"
 
 module Blorgh
-  # Your code goes here...
+  # cattr_accessorと同じ
+  # 値をすべてのクラスで共有したい場合に使う
+  mattr_accessor :author_class
+
+  # author_classでの保存時にconstantizeが必ず呼び出されるようにしたい場合
+  # 以下のように実装する
+  def self.author_class
+    @@author_class.constantize
+  end
 end
